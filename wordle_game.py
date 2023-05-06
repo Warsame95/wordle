bcb861144fe5947da71e9ec2ed131b3086e6a8fc
Warsame <warsameahmed95@gmail.com>
2023-05-07 00:51:14 +0100

    created basic helper functions

--- wordle_game.py
+++ wordle_game.py
@@ -1,12 +1,23 @@
 import pathlib
+import random
 
 WORDLIST = pathlib.Path("word_list.txt")
 
 word_list = [word.upper()
  for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")]
 
-print(word_list[1])
 
+    
+def answer():
+    return random.choice(word_list)
+
+
+def check_guess(guess, answer):
+    if guess == answer:
+        return True
+    return False
+
+answer = answer()
 
 for i in range(6):
 
@@ -14,10 +25,9 @@ for i in range(6):
         guess = input("guess a word\n")
 
         if len(guess) == 5:
+            check_guess(guess, answer)
             break
+        
         else:
             print("Not a valid entry")
 
-
-
-    
